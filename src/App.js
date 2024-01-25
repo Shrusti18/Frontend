486bedd10b8dfe1f12a35170aef8d1f64d9f04d6
Shrusti18 <shrusti.bali@atdxt.com>
2024-01-25 17:16:30 +0530

    frontend pushed now

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './layout/Navbar';
+import Page from './page/Page';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import AddStudent from './student/AddStudent';
+import EditStudent from './student/EditStudent';
+import ViewStudent from './student/ViewStudent';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route exact path='/' element={<Page />} />
+          <Route exact path='/addstudent' element={<AddStudent />} />
+          <Route exact path='/editstudent/:id' element={<EditStudent/>}/>
+          <Route exact path='/viewstudent/:id' element={<ViewStudent/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
 
 export default App;
+
